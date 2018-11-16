58e80f6a8f6c163e5cdb99873e8dc99a1c762b6e
Gerardo Nazarett <gdnazarett@student.fullsail.edu>
2018-11-15 21:07:41 -0500

    Initial Commit

---
+++ DIADAssitantiPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DIADAssitantiPad
+//
+//  Created by Gerardo Nazarett on 11/15/18.
+//  Copyright © 2018 Gerardo Nazarett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
